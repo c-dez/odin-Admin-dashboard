aff28d5fbb51443f38379e459bf271e63cd38ef9
cats <cats@pop-os.localdomain>
2023-07-04 23:08:23 -0600

    added cards content/js

---
+++ ap.js
@@ -0,0 +1,45 @@
+console.log('hello')
+//card = text content of .main
+//card1
+let card1_Title = document.querySelector('.card>#card1-title')
+card1_Title.textContent = 'Este es un titulo!'
+
+let card1_Text = document.querySelector('.card>#card1-text')
+card1_Text.textContent = '"Lorem ipsum dolor sit amet,Fusce cursus'
+//cadr2
+
+let card2_Title = document.querySelector('.card>#card2-title')
+card2_Title.textContent = 'Segundo titulo'
+
+let card2_Text = document.querySelector('.card>#card2-text')
+card2_Text.textContent = '"Lorem ipsum dolor sit ame orci a'
+//card3
+let card3_Title = document.querySelector('.card>#card3-title')
+card3_Title.textContent = 'Tercer titulo'
+
+let card3_Text = document.querySelector('.card>#card3-text')
+card3_Text.textContent = '"Lorem ipsum dolor sirci at risus dignissim consequat. Suspendisse potenti. Sed consectetur neque eu pulvinar scelerisque. Integer tincidunt mauris nec dolor tempus, ac blandit justo feugiat.'
+
+//card4
+let card4_Title = document.querySelector('.card>#card4-title')
+card4_Title.textContent = 'cuarto titulo'
+
+let card4_Text = document.querySelector('.card>#card4-text')
+card4_Text.textContent = '"Lorem ipsum dolor sirci at risus dignissim consequat. Suspendisse potenti. Sed consectetur neque eu pulvinar scelerisque. Integer tincidunt mauris nec dolor tempus, ac blandit justo feugiat.'
+
+//card5
+let card5_Title = document.querySelector('.card>#card5-title')
+card5_Title.textContent = 'cinco haha'
+
+let card5_Text = document.querySelector('.card>#card5-text')
+card5_Text.textContent = '"Lorem ipsum dolor sirci at risus dignissim consequat. Suspendisse potenti. Sed consectetur neque eu pulvinar scelerisque. Integer tincidunt mauris nec dolor tempus, ac blandit justo feugiat.'
+
+//card6
+let card6_Title = document.querySelector('.card>#card6-title')
+card6_Title.textContent = 'Six titulo'
+
+let card6_Text = document.querySelector('.card>#card6-text')
+card6_Text.textContent = '"Lorem ipsum dolor sirci at risus dignissim consequat. Suspendisse potenti. Sed consectetur neque eu pulvinar scelerisque. Integer tincidunt mauris nec dolor tempus, ac blandit justo feugiat.'
+
+
+
